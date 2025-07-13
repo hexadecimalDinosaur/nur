2824f8e300f5249e02cb13cba1a612942fa043e6
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-07-13 00:37:57 -0400

    xasm: init at 1.2.0-unstable-2025-06-21

--- overlays/python.nix
+++ overlays/python.nix
@@ -7,6 +7,7 @@ final: prev: {
       lib3to6 = pyfinal.callPackage ../pkgs/lib3to6/default.nix { };
       markdown-katex = pyfinal.callPackage ../pkgs/markdown-katex/default.nix { inherit lib3to6; };
       pyinstxtractor-ng = pyfinal.callPackage ../pkgs/pyinstxtractor-ng/default.nix { inherit xdis; };
+      xasm = pyfinal.callPackage ../pkgs/xasm/default.nix { inherit xdis x-python; };
       xdis = pyprev.xdis.overrideAttrs (final: old: {
         patches = [
           ../pkgs/xdis/xdis.patch
