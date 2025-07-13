ea6423d3fa768c12ceb5159db8336630348c99e6
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-07-12 20:56:47 -0400

    android-unpinner: init at 202107.1047

---
+++ pkgs/android-unpinner/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  rich-click,
+}:
+
+buildPythonPackage {
+  pname = "android-unpinner";
+  version = "202107.1047";
+
+  src = fetchFromGitHub {
+    owner = "mitmproxy";
+    repo = "android-unpinner";
+    rev = "f5ced37a9e40740d160faef9491121ba2df48ffe";
+    hash = "sha256-ZGUj5d47rEovpz7Wz25670OPaxT1mA2st7+L6Nz1G3w=";
+  };
+
+  doCheck = false;
+
+  dependencies = [
+    rich-click
+  ];
+
+  pyproject = true;
+  build-system = [
+    setuptools
+  ];
+}
